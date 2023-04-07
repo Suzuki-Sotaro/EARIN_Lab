8133175f211239476b1c25ec5dde70921b6236ae
Suzuki-Sotaro <39415489+Suzuki-Sotaro@users.noreply.github.com>
2023-04-07 19:21:58 +0200

    説明

--- Lab3.py
+++ Lab3.py
@@ -2,25 +2,30 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
+# Define the Bukin function
 def bukin_function(x, y):
     return 100 * np.sqrt(np.abs(y - 0.01 * x ** 2)) + 0.01 * np.abs(x + 10)
 
+# Create the initial population within the given search space
 def create_initial_population(mu, x_range, y_range):
     population = np.column_stack([np.random.uniform(x_range[0], x_range[1], mu),
                                   np.random.uniform(y_range[0], y_range[1], mu)])
     return population
 
+# Tournament selection for parent selection
 def tournament_selection(population, fitness_values, k=2):
     indices = np.random.choice(len(population), size=k, replace=False)
     best_index = np.argmin(fitness_values[indices])
     return population[indices[best_index]]
 
+# Gaussian mutation with mutation probability
 def mutate(parent, mutation_strength, mutation_probability):
     mutation_flag = np.random.rand(2) < mutation_probability
     mutation_noise = np.random.normal(0, mutation_strength, 2) * mutation_flag
     offspring = parent + mutation_noise
     return offspring
 
+# Plot the population for visualization
 def plot_population(population, generation):
     plt.scatter(population[:, 0], population[:, 1], label=f'Generation {generation}')
     plt.xlim(x_range)
@@ -32,29 +37,41 @@ def plot_population(population, generation):
     plt.pause(0.1)
     plt.clf()
 
+# Evolutionary strategy optimization algorithm
 def evolutionary_strategy(mu, lambd, generations, x_range, y_range, mutation_strength, mutation_probability, visualize=False):
+    # Generate the initial population within the given search space
     population = create_initial_population(mu, x_range, y_range)
     
+    # Iterate through the specified number of generations
     for generation in range(1, generations+1):
         offspring_population = []
         
+        # Generate lambda offspring by applying mutation to the mu parents
         for _ in range(lambd):
+            # Select a parent from the population using tournament selection
             parent = tournament_selection(population, np.array([bukin_function(x, y) for x, y in population]))
+            # Generate a new offspring by applying Gaussian mutation to the parent with the given mutation probability
             offspring = mutate(parent, mutation_strength, mutation_probability)
             offspring_population.append(offspring)
         
+        # Combine the offspring population with the parent population
         offspring_population = np.array(offspring_population)
         combined_population = np.vstack([population, offspring_population])
         
+        # Calculate the fitness values for the combined population
         fitness_values = np.array([bukin_function(x, y) for x, y in combined_population])
+        # Select the top mu individuals from the combined population based on their fitness values
         best_individuals_indices = np.argsort(fitness_values)[:mu]
         population = combined_population[best_individuals_indices]
 
+        # Visualize the optimization process if the option is enabled
         if visualize:
             plot_population(population, generation)
     
+    # Return the best solution from the final population as the optimal solution
     best_solution = population[np.argmin(fitness_values[:mu])]
     return best_solution
+
 
 # Parameters
 mu = 50
@@ -71,9 +88,10 @@ start_time = time.time()
 optimal_solution = evolutionary_strategy(mu, lambd, generations, x_range, y_range, mutation_strength, mutation_probability, visualize)
 optimization_time = time.time() - start_time
 
+# Output the results
 print("Optimal solution:", optimal_solution)
-print(bukin_function(optimal_solution[0], optimal_solution[1]))
 print("Optimization time (excluding visualization):", optimization_time, "seconds")
+
 
 
 """
