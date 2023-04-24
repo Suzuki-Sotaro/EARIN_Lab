1a99eac1a7201061c6a2517fc1abfcb3594a3667
Suzuki-Sotaro <39415489+Suzuki-Sotaro@users.noreply.github.com>
2023-04-24 13:54:24 +0200

    提出したコード

--- Lab3.py
+++ Lab3.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import argparse
+import csv
 
 # Define the Bukin function
 def bukin_function(x, y):
@@ -73,31 +74,7 @@ def evolutionary_strategy(mu, lambd, generations, x_range, y_range, mutation_str
     best_solution = population[np.argmin(fitness_values[:mu])]
     return best_solution
 
-
-# # Parameters
-# mu = 50
-# lambd = 100
-# generations = 200
-# x_range = (-15, -5)
-# y_range = (-3, 3)
-# mutation_strength = 0.5
-# mutation_probability = 0.9
-# visualize = True
-
-import csv
-
-# Option-1 Parameters to test
-mu_values = [10, 50, 100]
-lambd_values = [20, 100, 200]
-mutation_strength_values = [0.1, 0.5, 1.0]
-mutation_probability_values = [0.5, 0.9, 0.99]
-generations = 200
-x_range = (-15, -5)
-y_range = (-3, 3)
-# Disable visualization for parameter testing
-visualize = False
-
-# Option-2 Parse terminal arguments
+# Parse terminal arguments
 parser = argparse.ArgumentParser(description='Evolutionary Strategy Optimization for the Bukin Function')
 parser.add_argument('--mu', type=int, default=50, help='Number of parents (default: 50)')
 parser.add_argument('--lambd', type=int, default=100, help='Number of offspring (default: 100)')
@@ -110,6 +87,8 @@ parser.add_argument('--mutation_strength', type=float, default=0.5, help='Mutati
 parser.add_argument('--mutation_probability', type=float, default=0.9, help='Mutation probability (default: 0.9)')
 parser.add_argument('--visualize', action='store_true', help='Enable visualization (default: False)')
 args = parser.parse_args()
+
+# Assign the parsed arguments to variables
 mu = args.mu
 lambd = args.lambd
 generations = args.generations
@@ -118,9 +97,6 @@ y_range = (args.y_min, args.y_max)
 mutation_strength = args.mutation_strength
 mutation_probability = args.mutation_probability
 visualize = args.visualize
-# Example
-# python main.py --mu 50 --lambd 100 --generations 200 --x_min -15 --x_max -5 --y_min -3 --y_max 3 --mutation_strength 0.5 --mutation_probability 0.9
-
 
 # CSV file to save results
 csv_file = "results.csv"
@@ -132,77 +108,15 @@ with open(csv_file, mode='w', newline='') as file:
     # Write header row
     csv_writer.writerow(['mu', 'lambd', 'mutation_strength', 'mutation_probability', 'optimal_solution_x', 'optimal_solution_y', 'bukin_function(optimal_solution[0], optimal_solution[1])', 'optimization_time'])
 
-    for mu in mu_values:
-        for lambd in lambd_values:
-            for mutation_strength in mutation_strength_values:
-                for mutation_probability in mutation_probability_values:
-                    # Measure optimization time and optimize the Bukin function with the current parameter combination
-                    start_time = time.time()
-                    optimal_solution = evolutionary_strategy(mu, lambd, generations, x_range, y_range, mutation_strength, mutation_probability, visualize)
-                    optimization_time = time.time() - start_time
+    # Perform the optimization with the parsed arguments
+    start_time = time.time()
+    optimal_solution = evolutionary_strategy(mu, lambd, generations, x_range, y_range, mutation_strength, mutation_probability, visualize)
+    optimization_time = time.time() - start_time
 
-                    # Write the result row
-                    csv_writer.writerow([mu, lambd, mutation_strength, mutation_probability, optimal_solution[0], optimal_solution[1], bukin_function(optimal_solution[0], optimal_solution[1]), optimization_time])
-                    print(f"Parameters: mu={mu}, lambd={lambd}, mutation_strength={mutation_strength}, mutation_probability={mutation_probability}")
-                    print("Optimal solution:", optimal_solution)
-                    print("Optimization time (excluding visualization):", optimization_time, "seconds")
-                    print("----------------------------------------------------")
-                    
-                    # Calculate the fitness value of the optimal solution
-                    fitness_value = bukin_function(optimal_solution[0], optimal_solution[1])
+    # Write the result row
+    csv_writer.writerow([mu, lambd, mutation_strength, mutation_probability, optimal_solution[0], optimal_solution[1], bukin_function(optimal_solution[0], optimal_solution[1]), optimization_time])
 
-                    # Store the result in the results list
-                    result = {'mu': mu, 'lambd': lambd, 'mutation_strength': mutation_strength, 'mutation_probability': mutation_probability,
-                              'optimal_solution': optimal_solution, 'fitness_value': fitness_value, 'optimization_time': optimization_time}
-                    results.append(result)
-
-
-# # Measure optimization time and optimize the Bukin function
-# start_time = time.time()
-# optimal_solution = evolutionary_strategy(mu, lambd, generations, x_range, y_range, mutation_strength, mutation_probability, visualize)
-# optimization_time = time.time() - start_time
-
-# # Output the results
-# print("Optimal solution:", optimal_solution)
-# print("Optimization time (excluding visualization):", optimization_time, "seconds")
-
-# Sort the results list based on fitness_value
-sorted_results = sorted(results, key=lambda x: x['fitness_value'])
-
-# Print the best result (lowest fitness value)
-best_result = sorted_results[0]
-print("Best Result:")
-print(f"Parameters: mu={best_result['mu']}, lambd={best_result['lambd']}, mutation_strength={best_result['mutation_strength']}, mutation_probability={best_result['mutation_probability']}")
-print("Optimal solution:", best_result['optimal_solution'])
-print("Fitness value:", best_result['fitness_value'])
-print("Optimization time (excluding visualization):", best_result['optimization_time'], "seconds")
-
-
-
-"""
-Parameters' explanation
-
-mu: This parameter represents the population size or the number of individuals in the population. 
-A larger population size generally increases the diversity of the solutions being explored, which can help in avoiding local optima. However, larger populations also increase the computational cost of the algorithm.
-
-lambd: This parameter represents the offspring size or the number of offspring generated in each generation. 
-Larger offspring sizes provide more potential solutions to be explored, but they also increase the computational cost of the algorithm.
-
-generations: This parameter represents the number of generations the algorithm will run for. 
-The termination criterion for the algorithm is based on the number of generations. A larger number of generations allows the algorithm to explore more solutions, but it also increases the optimization time.
-
-x_range and y_range: These parameters define the search space's bounds for the x and y coordinates. 
-It is essential to set these ranges so that the global minimum of the Bukin function lies within the search space.
-
-mutation_strength: This parameter determines the strength of the Gaussian mutation applied to the parent's coordinates. 
-A higher mutation strength allows the algorithm to explore more distant solutions but might cause it to overshoot the optimal solution. 
-A lower value will result in more focused exploration, but convergence might be slower.
-
-mutation_probability: This parameter controls the probability of mutation happening for each coordinate of the offspring. 
-A higher mutation probability results in more frequent mutations, increasing the exploration of the search space. 
-However, it might also cause the search to be more random, potentially increasing the optimization time.
-
-visualize: This is a boolean parameter that controls whether the optimization process is visualized or not. 
-If set to True, the population of each generation will be plotted, showing the convergence of the algorithm. 
-Visualization helps to understand the optimization process but might also slow down the program's execution. If set to False, the visualization will be disabled, and the program will run faster.
-"""
+# Output the results
+print(f"Parameters: mu={mu}, lambd={lambd}, mutation_strength={mutation_strength}, mutation_probability={mutation_probability}")
+print("Optimal solution:", optimal_solution)
+print("Optimization time (excluding visualization):", optimization_time, "seconds")
--- Lab4.py
+++ Lab4.py
@@ -8,7 +8,8 @@ from xgboost import XGBRegressor, XGBClassifier
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-def predict_critic_rating(method='regression'):
+
+def load_and_preprocess_data():
     # Load the dataset
     data = pd.read_csv('variant3.csv')
 
@@ -21,18 +22,23 @@ def predict_critic_rating(method='regression'):
     # Handle missing values
     data['Time_taken'].fillna(data['Time_taken'].mean(), inplace=True)
 
-    if method == 'classification':
-        # Convert Critic_rating into discrete categories
-        # Convert Critic_rating into discrete categories
-        bins = [0, 7, 8, 10]
-        labels = [0, 1, 2]
-        data['Critic_rating'] = pd.cut(data['Critic_rating'], bins=bins, labels=labels)
+    return data
 
 
-    # Define the feature matrix and target variable
+def prepare_classification_data(data):
+    bins = [0, 7, 8, 10]
+    labels = [0, 1, 2]
+    data['Critic_rating'] = pd.cut(data['Critic_rating'], bins=bins, labels=labels)
+    return data
+
+
+def prepare_feature_matrix_and_target(data):
     X = data.drop(columns=['Critic_rating'])
     y = data['Critic_rating']
+    return X, y
 
+
+def split_and_standardize_data(X, y):
     # Split the dataset into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -41,103 +47,74 @@ def predict_critic_rating(method='regression'):
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
+    return X_train, X_test, y_train, y_test
+
+
+def regression_methods(X_train, X_test, y_train, y_test):
+    # Initialize models
+    models = {
+        "Linear Regression": LinearRegression(),
+        "Support Vector Regression": SVR(kernel='linear'),
+        "Random Forest Regression": RandomForestRegressor(),
+        "XGBoost Regression": XGBRegressor()
+    }
+
+    r2_scores = {}
+
+    # Train and evaluate each model
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        r2_scores[name] = r2
+        print(f"{name} - Mean Squared Error: {mse}, R-squared: {r2}")
+
+    # Find the best regression method
+    best_method = max(r2_scores, key=r2_scores.get)
+    print(f"The best regression method is: {best_method}")
+
+
+def classification_methods(X_train, X_test, y_train, y_test):
+    # Initialize models
+    models = {
+        "Logistic Regression": LogisticRegression(),
+        "Support Vector Machine Classifier": SVC(kernel='linear'),
+        "Random Forest Classifier": RandomForestClassifier(),
+        "XGBoost Classifier": XGBClassifier()
+    }
+
+    accuracies = {}
+
+    # Train and evaluate each model
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        report = classification_report(y_test, y_pred)
+        accuracies[name] = accuracy
+        print(f"{name} - Accuracy: {accuracy}\nClassification Report:\n{report}")
+
+    # Find the best classification method
+    best_method = max(accuracies, key=accuracies.get)
+    print(f"The best classification method is: {best_method}")
+
+
+def predict_critic_rating(method='regression'):
+    data = load_and_preprocess_data()
+
+    if method == 'classification':
+        data = prepare_classification_data(data)
+
+    X, y = prepare_feature_matrix_and_target(data)
+    X_train, X_test, y_train, y_test = split_and_standardize_data(X, y)
+
     if method == 'regression':
-        # Regression methods
+        regression_methods(X_train, X_test, y_train, y_test)
 
-        # Method 1: Linear Regression
-        linear_regression = LinearRegression()
-        linear_regression.fit(X_train, y_train)
-        y_pred_lr = linear_regression.predict(X_test)
-
-        # Method 2: Support Vector Machine Regression
-        svm_regression = SVR(kernel='linear')
-        svm_regression.fit(X_train, y_train)
-        y_pred_svr = svm_regression.predict(X_test)
-        
-        # Method 3: Random Forest Regressor
-        random_forest_regressor = RandomForestRegressor()
-        random_forest_regressor.fit(X_train, y_train)
-        y_pred_rf = random_forest_regressor.predict(X_test)
-
-        # Method 4: XGBoost Regressor
-        xgb_regressor = XGBRegressor()
-        xgb_regressor.fit(X_train, y_train)
-        y_pred_xgb = xgb_regressor.predict(X_test)
-
-        # Evaluate the performance
-        mse_lr = mean_squared_error(y_test, y_pred_lr)
-        mse_svr = mean_squared_error(y_test, y_pred_svr)
-        r2_lr = r2_score(y_test, y_pred_lr)
-        r2_svr = r2_score(y_test, y_pred_svr)
-        
-        # Evaluate the performance
-        mse_rf = mean_squared_error(y_test, y_pred_rf)
-        mse_xgb = mean_squared_error(y_test, y_pred_xgb)
-        r2_rf = r2_score(y_test, y_pred_rf)
-        r2_xgb = r2_score(y_test, y_pred_xgb)
-
-        print(f"Linear Regression - Mean Squared Error: {mse_lr}, R-squared: {r2_lr}")
-        print(f"Support Vector Regression - Mean Squared Error: {mse_svr}, R-squared: {r2_svr}")
-        print(f"Random Forest Regression - Mean Squared Error: {mse_rf}, R-squared: {r2_rf}")
-        print(f"XGBoost Regression - Mean Squared Error: {mse_xgb}, R-squared: {r2_xgb}")
-
-        # Find the best regression method
-        methods = ['Linear Regression', 'Support Vector Regression', 'Random Forest Regression', 'XGBoost Regression']
-        r2_scores = [r2_lr, r2_svr, r2_rf, r2_xgb]
-        best_method_index = np.argmax(r2_scores)
-        print(f"The best regression method is: {methods[best_method_index]}")
-
-            
     elif method == 'classification':
-        # Classification methods
+        classification_methods(X_train, X_test, y_train, y_test)
 
-        # Method 1: Logistic Regression
-        logistic_regression = LogisticRegression()
-        logistic_regression.fit(X_train, y_train)
-        y_pred_lr = logistic_regression.predict(X_test)
-
-        # Method 2: Support Vector Machine Classification
-        svm_classifier = SVC(kernel='linear')
-        svm_classifier.fit(X_train, y_train)
-        y_pred_svm = svm_classifier.predict(X_test)
-        
-        # Method 3: Random Forest Classifier
-        random_forest_classifier = RandomForestClassifier()
-        random_forest_classifier.fit(X_train, y_train)
-        y_pred_rf = random_forest_classifier.predict(X_test)
- 
-        # Method 4: XGBoost Classifier
-        xgb_classifier = XGBClassifier()
-        xgb_classifier.fit(X_train, y_train)
-        y_pred_xgb = xgb_classifier.predict(X_test)
-
-        # Evaluate the performance
-        accuracy_lr = accuracy_score(y_test, y_pred_lr)
-        accuracy_svm = accuracy_score(y_test, y_pred_svm)
-        report_lr = classification_report(y_test, y_pred_lr)
-        report_svm = classification_report(y_test, y_pred_svm)
-        
-        # Evaluate the performance
-        accuracy_rf = accuracy_score(y_test, y_pred_rf)
-        accuracy_xgb = accuracy_score(y_test, y_pred_xgb)
-        report_rf = classification_report(y_test, y_pred_rf)
-        report_xgb = classification_report(y_test, y_pred_xgb)
-
-        print(f"Logistic Regression - Accuracy: {accuracy_lr}\nClassification Report:\n{report_lr}")
-        print(f"Support Vector Machine Classifier - Accuracy: {accuracy_svm}\nClassification Report:\n{report_svm}")
-        print(f"Random Forest Classifier - Accuracy: {accuracy_rf}\nClassification Report:\n{report_rf}")
-        print(f"XGBoost Classifier - Accuracy: {accuracy_xgb}\nClassification Report:\n{report_xgb}")
-
-        # Find the best classification method
-        methods = ['Logistic Regression', 'Support Vector Machine Classifier', 'Random Forest Classifier', 'XGBoost Classifier']
-        accuracies = [accuracy_lr, accuracy_svm, accuracy_rf, accuracy_xgb]
-        best_method_index = np.argmax(accuracies)
-        print(f"The best classification method is: {methods[best_method_index]}")
-        
-        if accuracy_lr > accuracy_svm:
-            print("Logistic Regression is the better method.")
-        else:
-            print("Support Vector Machine Classifier is the better method.")
     else:
         print("Invalid method. Choose 'regression' or 'classification'.")
 
@@ -145,4 +122,3 @@ def predict_critic_rating(method='regression'):
 # Example usage:
 predict_critic_rating('regression')  # For regression methods
 predict_critic_rating('classification')  # For classification methods
-
