f33e9960b76772481b5480c3969b5c7960e62ae4
chii2000 <62940570+chii2000@users.noreply.github.com>
2023-05-06 13:07:34 +0200

    Update Lab4.py
    The complete program, as described, includes all the necessary functions for loading, preprocessing, and training machine learning models for regression and classification methods. The final `predict_critic_rating()` function ties everything together and displays the evaluation results. Finally, the program calls the `predict_critic_rating()` function with both "regression" and "classification" as arguments to execute and display the respective results.

--- Lab4.py
+++ Lab4.py
@@ -1,124 +1,119 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.svm import SVR, SVC
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from xgboost import XGBRegressor, XGBClassifier
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, classification_report
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+import xgboost as xgb
 
 
 def load_and_preprocess_data():
-    # Load the dataset
     data = pd.read_csv('variant3.csv')
 
-    # Preprocess the data
-    data['3D_available'] = data['3D_available'].apply(lambda x: 1 if x == 'YES' else 0)
-    data['Start_Tech_Oscar'] = data['Start_Tech_Oscar'].apply(lambda x: 1 if x == 'YES' else 0)
-    label_encoder = LabelEncoder()
-    data['Genre'] = label_encoder.fit_transform(data['Genre'])
+    data.drop(['time_taken', 'twitter_hastags', 'Num_multiplex'], axis=1, inplace=True)
 
-    # Handle missing values
-    data['Time_taken'].fillna(data['Time_taken'].mean(), inplace=True)
+    data['Total_Rating'] = data[
+        ['Lead_Actor_Rating', 'Lead_Actress_rating', 'Director_rating', 'Producer_rating', 'Critic_rating']].sum(axis=1)
+    data['Avg_Rating'] = data[
+        ['Lead_Actor_Rating', 'Lead_Actress_rating', 'Director_rating', 'Producer_rating', 'Critic_rating']].mean(
+        axis=1)
+    data['Budget_per_Second'] = data['Budget'] / data['Length']
+    data['Trailer_views_per_Day'] = data['Trailer_views'] / data['Days']
 
     return data
 
 
 def prepare_classification_data(data):
-    bins = [0, 7, 8, 10]
-    labels = [0, 1, 2]
+    bins = [0, 2.5, 5]
+    labels = ['Low', 'High']
     data['Critic_rating'] = pd.cut(data['Critic_rating'], bins=bins, labels=labels)
+    data['Critic_rating'] = LabelEncoder().fit_transform(data['Critic_rating'])
     return data
 
 
 def prepare_feature_matrix_and_target(data):
-    X = data.drop(columns=['Critic_rating'])
+    X = data.drop('Critic_rating', axis=1)
     y = data['Critic_rating']
     return X, y
 
 
 def split_and_standardize_data(X, y):
-    # Split the dataset into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Standardize the data
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
-
     return X_train, X_test, y_train, y_test
 
 
 def regression_methods(X_train, X_test, y_train, y_test):
-    # Initialize models
     models = {
-        "Linear Regression": LinearRegression(),
-        "Support Vector Regression": SVR(kernel='linear'),
-        "Random Forest Regression": RandomForestRegressor(),
-        "XGBoost Regression": XGBRegressor()
+        'Linear Regression': LinearRegression(),
+        'Support Vector Regression': SVR(),
+        'Random Forest Regression': RandomForestRegressor(),
+        'XGBoost Regression': xgb.XGBRegressor()
     }
 
-    r2_scores = {}
+    best_model = None
+    best_r2 = -np.inf
 
-    # Train and evaluate each model
     for name, model in models.items():
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         mse = mean_squared_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
-        r2_scores[name] = r2
-        print(f"{name} - Mean Squared Error: {mse}, R-squared: {r2}")
 
-    # Find the best regression method
-    best_method = max(r2_scores, key=r2_scores.get)
-    print(f"The best regression method is: {best_method}")
+        if r2 > best_r2:
+            best_model = name
+            best_r2 = r2
+
+        print(f"{name} - Mean Squared Error: {mse:.2f}, R-squared: {r2:.2f}")
+
+    print(f"Best Regression Model: {best_model}, R-squared: {best_r2:.2f}")
 
 
 def classification_methods(X_train, X_test, y_train, y_test):
-    # Initialize models
     models = {
-        "Logistic Regression": LogisticRegression(),
-        "Support Vector Machine Classifier": SVC(kernel='linear'),
-        "Random Forest Classifier": RandomForestClassifier(),
-        "XGBoost Classifier": XGBClassifier()
+        'Logistic Regression': LogisticRegression(),
+        'Support Vector Machine Classifier': SVC(),
+        'Random Forest Classifier': RandomForestClassifier(),
+        'XGBoost Classifier': xgb.XGBClassifier()
     }
 
-    accuracies = {}
+    best_model = None
+    best_accuracy = -np.inf
 
-    # Train and evaluate each model
     for name, model in models.items():
         model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred)
-        accuracies[name] = accuracy
-        print(f"{name} - Accuracy: {accuracy}\nClassification Report:\n{report}")
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    cls_report = classification_report(y_test, y_pred)
 
-    # Find the best classification method
-    best_method = max(accuracies, key=accuracies.get)
-    print(f"The best classification method is: {best_method}")
+    if acc > best_accuracy:
+        best_model = name
+        best_accuracy = acc
 
+    print(f"{name} - Accuracy: {acc:.2f}")
+    print(f"Classification Report:\n{cls_report}")
 
-def predict_critic_rating(method='regression'):
-    data = load_and_preprocess_data()
-
-    if method == 'classification':
+print(f"Best Classification Model: {best_model}, Accuracy: {best_accuracy:.2f}")
+def predict_critic_rating(method):
+data = load_and_preprocess_data()
+    if method == "classification":
         data = prepare_classification_data(data)
 
     X, y = prepare_feature_matrix_and_target(data)
     X_train, X_test, y_train, y_test = split_and_standardize_data(X, y)
 
-    if method == 'regression':
+    if method == "regression":
         regression_methods(X_train, X_test, y_train, y_test)
-
-    elif method == 'classification':
+    elif method == "classification":
         classification_methods(X_train, X_test, y_train, y_test)
-
     else:
-        print("Invalid method. Choose 'regression' or 'classification'.")
-
-
-# Example usage:
-predict_critic_rating('regression')  # For regression methods
-predict_critic_rating('classification')  # For classification methods
+        raise ValueError("Invalid method. Choose 'regression' or 'classification'")
+if name == "main":
+print("Regression Results:")
+predict_critic_rating("regression")
+print("\nClassification Results:")
+predict_critic_rating("classification")
